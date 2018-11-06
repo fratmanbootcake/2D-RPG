56faba84a5066948c815bacfdf8267b935fa60cb
fratmanbootcake <35295328+fratmanbootcake@users.noreply.github.com>
2018-11-06 19:14:23 +0000

    Update Player.py
    Added in diagonal movement and included smooth sliding around walls.

--- Player.py
+++ Player.py
@@ -22,45 +22,67 @@ class Player(pygame.sprite.Sprite):
             self.vx = -PLAYER_SPEED
         elif keys[pygame.K_RIGHT]:
             self.vx = PLAYER_SPEED
-        elif keys[pygame.K_UP]:
+        if keys[pygame.K_UP]:
             self.vy = -PLAYER_SPEED
         elif keys[pygame.K_DOWN]:
-            self.vy = PLAYER_SPEED        
+            self.vy = PLAYER_SPEED
 
-    def collides(self):
+        if self.vx != 0 and self.vy != 0:
+            self.vx = 0.7101 * self.vx
+            self.vy = 0.7101 * self.vy   
+
+    def collisions(self):
         new_x = self.x + self.vx * self.game.dt
         new_y = self.y + self.vy * self.game.dt
         for sprite in self.game.all_sprites:
-            if pygame.Rect(new_x, new_y, TILE, TILE).colliderect(sprite):
+            if sprite in self.game.walls:
+                if pygame.Rect(new_x, new_y, TILE, TILE).colliderect(sprite):
                     return sprite
-            
-    def position_update(self, sprite, rebound):
-        if self.vx > 0:
-            self.x = sprite.x - rebound
-            self.vx = 0
-        elif self.vx < 0:
-            self.x = sprite.x + rebound
-            self.vx = 0
-        if self.vy > 0:
-            self.y = sprite.y - rebound
-            self.vy = 0
-        elif self.vy < 0:
-            self.y = sprite.y + rebound
-            self.vy = 0
-     
+
+    def x_collision(self):
+        new_x = self.x + self.vx * self.game.dt
+        for sprite in self.game.all_sprites:
+            if sprite in self.game.walls:
+                if pygame.Rect(new_x, self.y, TILE, TILE).colliderect(sprite):
+                    return sprite
+
+    def y_collision(self):
+        new_y = self.y + self.vy * self.game.dt
+        for sprite in self.game.all_sprites:
+            if sprite in self.game.walls:
+                if pygame.Rect(self.x, new_y, TILE, TILE).colliderect(sprite):
+                    return sprite
+
+    def x_position_reset(self, sprite):
+        if sprite:
+            if self.vx > 0:
+                self.x = sprite.x - TILE
+                self.vx = 0
+            elif self.vx < 0:
+                self.x = sprite.x + TILE
+                self.vx = 0
+                
+    def y_position_reset(self, sprite):
+        if sprite:
+            if self.vy > 0:
+                self.y = sprite.y - TILE
+                self.vy = 0
+            elif self.vy < 0:
+                self.y = sprite.y + TILE
+                self.vy =0
+
+    def move(self): 
+        self.x = self.x + self.vx * self.game.dt
+        self.y = self.y + self.vy * self.game.dt
+    
     def update(self):
         self.handle_event()
-        if self.collides():
-            sprite = self.collides()
-            if sprite in self.game.walls:
-                self.position_update(sprite, TILE)
-            elif sprite in self.game.monsters:
-                self.position_update(sprite, 2*TILE)
-            #elif sprite in self.game.items:
-                # some update for collecting items
-        else:
-            self.x = self.x + self.vx * self.game.dt
-            self.y = self.y + self.vy * self.game.dt
+        if self.collisions():
+            if self.x_collision():
+                self.x_position_reset(self.x_collision())
+            if self.y_collision():
+                self.y_position_reset(self.y_collision())
+        self.move()
             
 
 
