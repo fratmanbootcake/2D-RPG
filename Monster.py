0ac6f0415d9f4d3b6c779e653992cf0d697fa1e8
fratmanbootcake <35295328+fratmanbootcake@users.noreply.github.com>
2018-11-10 21:33:25 +0000

    Update Monster.py
    Monster now inherits from the Entity class.

--- Monster.py
+++ Monster.py
@@ -1,26 +1,45 @@
 import pygame as pygame
 from Constants import *
 
-class Monster(pygame.sprite.Sprite):
+class Monster(Entity):
 
     def __init__(self, game, x, y):
-        self.game = game
-        self.groups = game.all_sprites, game.walls
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((TILE, TILE))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.x = x * TILE
-        self.y = y * TILE
-        self.rect.x = x * TILE
-        self.rect.y = y * TILE
-        self.starting_health = 10
-        self.current_health = self.starting_health
+        super().__init__(game, x, y)
 
-    def damage(self):
-        self.current_health -= 1
-        if self.current_health == 0:
-            self.kill()
+    def load_images(self):
+        self.south_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_fr1.gif")).convert())
+        self.south_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_fr2.gif")).convert())
+        self.west_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_lf1.gif")).convert())
+        self.west_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_lf2.gif")).convert())
+        self.east_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_rt1.gif")).convert())
+        self.east_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_rt2.gif")).convert())
+        self.north_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_bk1.gif")).convert())
+        self.north_images.append(pygame.image.load(os.path.join(image_folder,"dvl1_bk2.gif")).convert())
+        
+        for image in self.south_images:
+            image.set_colorkey(pygame.Color('#ffffff'))
+        for image in self.north_images:
+            image.set_colorkey(pygame.Color('#ffffff'))
+        for image in self.east_images:
+            image.set_colorkey(pygame.Color('#ffffff'))
+        for image in self.west_images:
+            image.set_colorkey(pygame.Color('#ffffff'))
+
+        self.image = self.south_images[1]
+
+    def try_move(self):
+        self.vx = PLAYER_SPEED/2
+        # until pathfinding is implemented (if required? maybe pokemon / FF style fights)
+
+    def update(self):
+        self.try_move()
+        self.update_facing()
+        if self.collisions():
+            if self.x_collision():
+                self.x_position_reset(self.x_collision())
+            if self.y_collision():
+                self.y_position_reset(self.y_collision())
+        self.move()
         
 
 
